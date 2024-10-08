a05fc38aff9388a364c82a47ec1fe878850991d0
Arthur Bertoti <artgbertoti@gmail.com>
2024-10-08 15:44:56 -0300

    initial commit

---
+++ src/notification/notification.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@nestjs/common';
+import { CreateNotificationDto } from './dto/create-notification.dto';
+import { UpdateNotificationDto } from './dto/update-notification.dto';
+import * as firebase from 'firebase-admin';
+import { sendNotificationDTO } from './dto/send-notification.dto';
+
+@Injectable()
+export class NotificationService {
+  async sendPush(notification: sendNotificationDTO) {
+    try {
+      console.log(notification);
+      await firebase
+        .messaging()
+        .send({
+          notification: {
+            title: notification.title,
+            body: notification.body,
+          },
+          token: notification.deviceId,
+          data: {},
+          android: {
+            priority: 'high',
+            notification: {
+              sound: 'default',
+              channelId: 'default',
+            },
+          },
+          webpush: { data: {}, headers: {}, notification: {}, fcmOptions: {} },
+          apns: {
+            headers: {
+              'apns-priority': '10',
+            },
+            payload: {
+              aps: {
+                contentAvailable: true,
+                sound: 'default',
+              },
+            },
+          },
+        })
+        .catch((error: any) => {
+          console.error(error);
+        });
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
+  }
+
+  create(createNotificationDto: CreateNotificationDto) {
+    return 'This action adds a new notification';
+  }
+
+  findAll() {
+    return `This action returns all notification`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} notification`;
+  }
+
+  update(id: number, updateNotificationDto: UpdateNotificationDto) {
+    return `This action updates a #${id} notification`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} notification`;
+  }
+}
